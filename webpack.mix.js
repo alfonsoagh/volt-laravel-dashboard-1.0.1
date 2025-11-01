6982e2f89362321004895091748f3d3e5477b2eb
alfonsoagh <alfonsoagh777@gmail.com>
2025-11-01 17:12:01 -0600

    Migración completa

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,4 +18,35 @@ const mix = require('laravel-mix');
  
  // Compile Volt SCSS (enables overriding variables in resources/scss/custom/_variables.scss)
  mix.sass('resources/scss/volt.scss', 'public/css')
-    .options({ processCssUrls: false });
+   .options({ processCssUrls: false });
+
+// Silence Sass deprecation warnings coming from node_modules (Bootstrap, etc.)
+mix.override((config) => {
+    const ensureQuietDeps = (useArray) => {
+        if (!Array.isArray(useArray)) return;
+        useArray.forEach((u) => {
+            if (u && u.loader && u.loader.includes('sass-loader')) {
+                u.options = {
+                    ...(u.options || {}),
+                    sassOptions: {
+                        ...(u.options && u.options.sassOptions ? u.options.sassOptions : {}),
+                        quietDeps: true,
+                        silenceDeprecations: [
+                            'legacy-js-api',
+                            'import',
+                            'global-builtin',
+                            'color-functions'
+                        ],
+                    },
+                };
+            }
+        });
+    };
+
+    (config.module.rules || []).forEach((rule) => {
+        if (rule.oneOf) {
+            rule.oneOf.forEach((one) => ensureQuietDeps(one.use));
+        }
+        ensureQuietDeps(rule.use);
+    });
+});
