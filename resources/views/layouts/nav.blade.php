dc7a97e25bafd368e7a4e50920634e8af1193b39
alfonsoagh <alfonsoagh777@gmail.com>
2025-11-02 22:15:52 -0600

    Revert "first commit"
    This reverts commit 66ef782f52373709a1d57aa2b1ba9a51e142d7bd.

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,17 +1,7 @@
  
 <nav class="navbar navbar-dark navbar-theme-primary px-4 col-12 d-md-none">
-    @php
-        $sintekLogo = 'assets/img/brand/sintek.png';
-        $hasSintek = file_exists(public_path($sintekLogo));
-    @endphp
-    <a class="navbar-brand me-lg-5" href="/index.html" title="SinTek">
-        @if ($hasSintek)
-            <img class="navbar-brand-dark" src="{{ asset($sintekLogo) }}" alt="SinTek logo" />
-            <img class="navbar-brand-light" src="{{ asset($sintekLogo) }}" alt="SinTek logo" />
-        @else
-            <img class="navbar-brand-dark" src="{{ asset('assets/img/brand/light.svg') }}" alt="Volt logo" />
-            <img class="navbar-brand-light" src="{{ asset('assets/img/brand/dark.svg') }}" alt="Volt logo" />
-        @endif
+    <a class="navbar-brand me-lg-5" href="/index.html">
+        <img class="navbar-brand-dark" src="{{ asset('assets/img/brand/light.svg') }}" alt="Volt logo" /> <img class="navbar-brand-light" src="{{ asset('assets/img/brand/dark.svg') }}" alt="Volt logo" />
     </a>
     <div class="d-flex align-items-center">
         <button class="navbar-toggler d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
